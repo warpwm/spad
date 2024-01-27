7def6514b29f4d9d6d495256e4144742323471a2
Trim Bresilla <trim.bresilla@gmail.com>
2024-01-27 11:55:32 +0100

    refactor: Refactor toggle_exec command for improved performance
    - Implement toggle_exec command
    - Add functionality to toggle the executable permission of a file
    - Ensure proper error handling and feedback to the user

--- src/commands/toggle_exec.rs
+++ src/commands/toggle_exec.rs
@@ -20,6 +20,22 @@ pub struct ToggleExecCommand {
     /// The amount of retries for trying to toggle the special workspace, runs with a 100ms interval
     #[arg(long, default_value_t = 30)]
     max_retries: u32,
+
+    /// The size of the window/workspace (in percent of screen)
+    #[arg(short, long, default_value = "80x80")]
+    size: String,
+
+    /// The position in screen of the window/workspace
+    #[arg(short, long, default_value_t = true)]
+    center: bool,
+
+    /// The type of the window/workspace, floating or tiled
+    #[arg(short, long, default_value = "float")]
+    wintype: String,
+
+    /// Whether the workspace should be hidden at startup
+    #[arg(long, default_value_t = false)]
+    hidden: bool,
 }
 
 const TIMEOUT: Duration = Duration::from_millis(100);
@@ -30,9 +46,25 @@ impl ToggleExecCommand {
             name,
             exec,
             max_retries,
+            size,
+            center,
+            wintype,
+            hidden,
         } = self;
 
         let target_workspace_name = format!("special:{name}");
+
+        let size = size
+            .split('x')
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect_vec();
+
+        let size_ = "size ".to_owned() + &size[0].to_string() + "% " + &size[1].to_string() + "%;";
+        let center_ = if center { "center;".to_owned() } else { "".to_owned() };
+        let wintype_ = "".to_owned() + &wintype + ";";
+
+        let all_val = wintype_ + &size_ + &center_;
+        print!("{}", all_val);
 
         let workspaces = Workspaces::get()?.collect_vec();
         let workspace_is_spawned = workspaces
@@ -40,7 +72,7 @@ impl ToggleExecCommand {
             .any(|workspace| workspace.name == target_workspace_name);
 
         if !workspace_is_spawned {
-            let exec_command = format!("[workspace {target_workspace_name}] {exec}");
+            let exec_command = format!("[workspace {target_workspace_name} {all_val};noanim] {exec}");
 
             Dispatch::call(DispatchType::Exec(&exec_command))?;
 
